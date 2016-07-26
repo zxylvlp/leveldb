42137808ee6ff53933ffcaba4b5a620ca7db6231
zxylvlp <zhaoxingyu19920404@gmail.com>
2016-07-26 10:40:05 +0800

    add comment of arena
    这里没有分析test_case因为需要提前分析测试夹具的代码，测试代码的分析也是很重要的，后面我还会做补充。

--- util/arena.cc
+++ util/arena.cc
@@ -7,19 +7,24 @@
 
 namespace leveldb {
 
+// 这里设定申请的块大小是4096
 static const int kBlockSize = 4096;
 
+// 构造函数将三个基本数据成员初始化，vector会调用默认的构造函数初始化，在这里我们不用管它
 Arena::Arena() : memory_usage_(0) {
   alloc_ptr_ = NULL;  // First allocation will allocate a block
   alloc_bytes_remaining_ = 0;
 }
 
+// 析构的时候需要将申请的资源释放掉，其他基本数据成员可以不用管
 Arena::~Arena() {
   for (size_t i = 0; i < blocks_.size(); i++) {
     delete[] blocks_[i];
   }
 }
 
+// 分配内存的后备它肯定会分配一个块, 4096/4则独立分配一块内存块，否则利用当前的内存块分配内存
+// 如果要分配的对象比较大的时候，一个块放不了多少东西，就会造成空间的浪费，就好像9/10 > 3/4一样。
 char* Arena::AllocateFallback(size_t bytes) {
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
@@ -38,9 +43,16 @@ char* Arena::AllocateFallback(size_t bytes) {
   return result;
 }
 
+// 分配对齐内存，这里的处理还是非常的有意思，
+// 我们要先计算bytes离对齐还有多远，如果指针大于8则要将对齐目标align设置为指针大小，否则就8字节对齐，
+// 将当前_ptr的后log2(align)位取出来作为current_mod，也就是对齐的mod数，
+// 然后取mod得补作为slop，
+// 如果在当前块中能分配bytes + slop的话则分配这么大，返回指针中也要加上slop，
+// 如果当前块分配不了则调AllocateFallback分配bytes内存，gcc可以自动保证对齐
 char* Arena::AllocateAligned(size_t bytes) {
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
   assert((align & (align-1)) == 0);   // Pointer size should be a power of 2
+  //uintptr_t是可以容纳指针大小的uint类型
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align-1);
   size_t slop = (current_mod == 0 ? 0 : align - current_mod);
   size_t needed = bytes + slop;
@@ -57,6 +69,9 @@ char* Arena::AllocateAligned(size_t bytes) {
   return result;
 }
 
+// 实际分配内存的地方，将分配出来的内存指针放到blocks_中以备将来释放，
+// 并将memory_usage_，加上分配的大小+一个指针的大小，这样做我猜测是加上了内存对齐的影响,
+// 然后返回刚刚分配内存的指针
 char* Arena::AllocateNewBlock(size_t block_bytes) {
   char* result = new char[block_bytes];
   blocks_.push_back(result);
--- util/arena.h
+++ util/arena.h
@@ -12,7 +12,8 @@
 #include "port/port.h"
 
 namespace leveldb {
-
+// Allocate AllocateAligned的责任是分配内存，从当前块中能分就从当前块分，不能就调AllocateFallback分配一个新块
+// AllocateFallback的责任是分配内存块，分配多大的内存块由它决定，它分配块的具体操作则由AllocateNewBlock完成
 class Arena {
  public:
   Arena();
@@ -26,6 +27,7 @@ class Arena {
 
   // Returns an estimate of the total memory usage of data allocated
   // by the arena.
+  // 返回当前使用内存的估计值
   size_t MemoryUsage() const {
     return reinterpret_cast<uintptr_t>(memory_usage_.NoBarrier_Load());
   }
@@ -35,13 +37,17 @@ class Arena {
   char* AllocateNewBlock(size_t block_bytes);
 
   // Allocation state
+  // 存放当前分配块中下面可以分配的位置
   char* alloc_ptr_;
+  // 存放当前分配块中还剩下的bytes数
   size_t alloc_bytes_remaining_;
 
   // Array of new[] allocated memory blocks
+  // 存放所有分配出来的内存块，以备将来释放
   std::vector<char*> blocks_;
 
   // Total memory usage of the arena.
+  // 存放内存的使用情况，不过为什么会用AtomicPointer这个类型还有待探讨。
   port::AtomicPointer memory_usage_;
 
   // No copying allowed
@@ -49,6 +55,7 @@ class Arena {
   void operator=(const Arena&);
 };
 
+// 分配内存如果能在当前块中分配则在当前块中分配，如果不能则调用AllocateFallback再分配一个块
 inline char* Arena::Allocate(size_t bytes) {
   // The semantics of what to return are a bit messy if we allow
   // 0-byte allocations, so we disallow them here (we don't need
