44613ea6b59e8f6f365c805ceaac4da3432f6405
zxylvlp <zhaoxingyu19920404@gmail.com>
2016-09-09 13:57:29 +0800

    finish repair

--- db/repair.cc
+++ db/repair.cc
@@ -76,6 +76,14 @@ class Repairer {
     }
   }
 
+  /**
+   * 运行修复
+   *
+   * 首先收集文件信息
+   * 然后将所有log文件转换为表文件
+   * 并且所有表文件提取meta信息
+   * 最后将所有信息都保存到描述符文件中
+   */
   Status Run() {
     Status status = FindFiles();
     if (status.ok()) {
@@ -212,9 +220,11 @@ class Repairer {
   }
 
   /**
-   * 转换日志文件到表格
+   * 转换日志文件列表到表格
    *
-   * 遍历日志文件号列表，
+   * 遍历日志文件号列表
+   * 对每一个日志号调ConvertLogToTable转换为表
+   * 最后调ArchiveFile将日志文件移动到lost目录
    */
   void ConvertLogFilesToTables() {
     for (size_t i = 0; i < logs_.size(); i++) {
@@ -229,6 +239,21 @@ class Repairer {
     }
   }
 
+  /**
+   * 转换日志文件到表格
+   *
+   * 首先声明一个日志报告者
+   * 利用日志文件号构造文件名，然后以顺序读打开日志文件
+   * 创建一个刚刚声明的日志报告者，将当前环境，当前选项中的info_log，和日志文件号分别传递进去
+   * 利用这个顺序读日志文件构造一个读者，将报告者传递进去
+   * 创建一个内存表
+   * 然后循环利用读者读取记录，然后将其插入内存表
+   * 最后析构日志文件
+   * 首先创建一个文件meta，然后将他的号码设置为下一个文件号，并且将下一个文件号加1
+   * 对内存表创建一个内存表的迭代器，利用这个迭代器调用BuildTable创建一个表文件
+   * 然后析构迭代器，对内存表去引用，并且置为空
+   * 最后将meta的文件号放到表序号列表中
+   */
   Status ConvertLogToTable(uint64_t log) {
     struct LogReporter : public log::Reader::Reporter {
       Env* env;
@@ -311,12 +336,23 @@ class Repairer {
     return status;
   }
 
+  /**
+   * 提取元数据
+   *
+   * 遍历表序号列表中的所有表序号
+   * 对其调用ScanTable扫描表的内容
+   */
   void ExtractMetaData() {
     for (size_t i = 0; i < table_numbers_.size(); i++) {
       ScanTable(table_numbers_[i]);
     }
   }
 
+  /**
+   * 根据meta创建一个表迭代器
+   *
+   * 从表缓存里面拿到文件号对应的迭代器
+   */
   Iterator* NewTableIterator(const FileMetaData& meta) {
     // Same as compaction iterators: if paranoid_checks are on, turn
     // on checksum verification.
@@ -325,6 +361,17 @@ class Repairer {
     return table_cache_->NewIterator(r, meta.number, meta.file_size);
   }
 
+  /**
+   * 根据文件号扫描表
+   *
+   * 首先构建一个表信息结构体，将其中meta的文件号设置为number
+   * 然后根据文件号获得表文件名，根据文件名查找到文件获得文件大小设置到meta中的文件大小上
+   * 如果文件大小获取失败则调用ArchiveFile移动文件到lost子目录并且返回
+   * 根据meta创建一个表的迭代器
+   * 然后迭代表中的数据，给表信息结构体设置最大序列号和meta中的开始和结束
+   * 最后析构迭代器
+   * 如果刚才的过程没有出错则将表信息加入到表信息列表中，否则调用RepairTable修复表
+   */
   void ScanTable(uint64_t number) {
     TableInfo t;
     t.meta.number = number;
@@ -388,6 +435,19 @@ class Repairer {
     }
   }
 
+  /**
+   * 根据文件名和表信息修复表
+   *
+   * 首先创建一个新文件，并且将下一个文件号加1
+   * 然后创建新文件的表构建者
+   * 然后迭代源文件中的所有数据，添加到构建者中，并且统计总数
+   * 然后将源文件移动到lost目录中
+   * 如果当前总数是0则调用构建者的取消方法
+   * 否则调用构建者的完成方法，并且给meta设置构建者中的文件大小
+   * 析构构建者，关闭新文件
+   * 如果刚刚统计的内容总数大于0，将新文件名改成原文件名，并且添加到表信息列表中
+   * 如果刚刚有出错的情况则删除新文件
+   */
   void RepairTable(const std::string& src, TableInfo t) {
     // We will copy src contents to a new table and then rename the
     // new table over the source.
@@ -442,6 +502,18 @@ class Repairer {
     }
   }
 
+  /**
+   * 写描述符文件
+   *
+   * 首先创建一个临时文件
+   * 获取文件信息列表中所有文件的最大序列号
+   * 设置edit_的日志号，下一个文件号，最大序列号和比较者
+   * 将每一个文件信息分别拆开添加到edit_中
+   * 对临时文件创建一个写者，并且将edit_编码并且保存到其中
+   * 如果有出错的情况则将临时文件删除
+   * 否则将所有描述符文件全部移动到lost目录中
+   * 并且将刚才的临时文件重命名为描述符文件，并且设置current指向这个文件
+   */
   Status WriteDescriptor() {
     std::string tmp = TempFileName(dbname_, 1);
     WritableFile* file;
@@ -502,7 +574,13 @@ class Repairer {
   }
 
   /**
-   * 路径名为fname的文件，移动到lost子目录中
+   * 将路径名为fname的文件，移动到lost子目录中
+   *
+   * 首先获得右边第一个/的位置
+   * 如果找到了则将新目录名加上这个位置之前的目录部分，否则新目录名置空
+   * 将新目录追加/lost，如果新目录不存在则创建
+   * 然后将新目录加上文件名，组成新文件名
+   * 最后将文件移动到新目录
    */
   void ArchiveFile(const std::string& fname) {
     // Move into another directory.  E.g., for
